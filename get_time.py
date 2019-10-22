b7c81d6be8515c576e2efce1e93c7e8f68f1ad84
joelantogit <56887075+joelantogit@users.noreply.github.com>
2019-10-23 01:42:56 +0800

    Update get_time.py
    added a function to get the day of the week

--- get_time.py
+++ get_time.py
@@ -4,4 +4,20 @@ print (time.ctime(time.time()))
 print ("year", currtime.tm_year)
 print ("month", currtime.tm_mon)
 print ("weekday", currtime.tm_wday)
+wday = currtime.tm_wday
 
+weekday = {
+			0: 'Monday',
+			1: 'Tuesday',
+			2: 'Wednesday',
+			3: 'Thurday',
+			4: 'Friday',
+			5: 'Saturday',
+			6: 'Sunday'
+		  }
+
+for day in weekday:
+	if (day == wday):
+		print ("day of the week is :", weekday[day])
+	else:
+		continue
