c34fa50e7ad8c84746b58b88b585a9d54ea4b1e0
joelantogit <56887075+joelantogit@users.noreply.github.com>
2019-11-06 14:42:53 +0800

    updates

--- inputdata.py
+++ inputdata.py
@@ -12,19 +12,22 @@ import json
 def user_input(arg): #to ask user and return data to fill in the json
 
 	possible_days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-	items = ["Mcspicy","Fish Burger","McBreakfast"]
 	count = 0
 	
 #to get the weekday
 	while (arg == 'day'):
 		count = count+1
 		try:
-			pointer = input("Please enter the day in full format eg; Sunday,Monday or enter 'X' to return :")
+			pointer = input("Please enter the day in full format eg; Sunday,Monday or enter 'X' to return or N for new day entry :")
 			for x in possible_days:
 				if (x == pointer ):
 					return pointer
 				elif (pointer == 'X'):
 					return 'exit'
+				elif (pointer == 'N'):
+					pointer = input("Please enter the name of the new day ")
+					stores.append(pointer)
+					return pointer
 			if(count >2):
 				return 'exit'
 			else:
@@ -65,8 +68,56 @@ def user_input(arg): #to ask user and return data to fill in the json
 				continue
 		except:
 			continue
+	while(arg == 'food_item'):
+		pointer = input("Please enter the food item name - :")
+		return pointer
+				
+
+
+	while(arg == 'fromtime'):
+		count = count +1
+		try:
+			pointer = input("Please enter the from time of item 09am,12pm - :")
+			x = pointer[0:2]
+			z=int(x)
+			y = pointer[2:4]
+			if x.isdigit() == True:
+				if(y=='am' or  y=='pm' and (0<=z<=12) ):
+					return pointer
+			else:
+				print ("Please enter in correct format")
+		except:
+			continue
+
+	while(arg == 'totime'):
+		count = count +1
+		try:
+			pointer = input("Please enter the to time of item 09am,12pm - :")
+			x = pointer[0:2]
+			z=int(x)
+			y = pointer[2:4]
+			if x.isdigit() == True:
+				if(y=='am' or  y=='pm' and (0<=z<=12) ):
+					return pointer
+			else:
+				print ("Please enter in correct format")
+		except:
+			continue
+	while(arg == 'price'):
+		count = count +1
+		try:
+			pointer = input("Please enter price  ")
+			x,y = pointer.split(".")
+			if ((x.isdigit() == True) and (y.isdigit() == True)):
+				z= float(pointer)
+				return z
+			else:
+				print ("Please enter price in correct format")
+		except:
+			continue
 	
-	item = input("enter the food item - Fish Burger, Mcspicy, Mbreakfast, Nasi Lemak, fried rice, Chicken rice, Paratha, dosa, Chapathi : ")
+
+
 	fromt = input("enter food item available from time in format : 9am,12pm : ")
 	tillt = input("enter food item available till time in format : 9am,12pm : ")
 	price = input("enter the price of the food item : ")
@@ -76,13 +127,17 @@ def user_input(arg): #to ask user and return data to fill in the json
 def input_to_json():
 	#opening file to read
 	file_handle = open ("test.json", mode="r", encoding="utf-8")
+	global file
 	file = json.load(file_handle)
 	file_handle.close()
 	#closed file
 
 	#####declaring variables
-	count = 0 #local count to use inside loops
+	stcount = 1 #local count to use inside loops
 	global stores
+	global food_items
+	food_items = []
+	
 	stores = []
 	for wday, jstore in file.items():
 		for keys, info in jstore.items():
@@ -96,54 +151,87 @@ def input_to_json():
 				}
 
 
-
+	#start input for day
 	global day
 	day = user_input('day')
 	if (day == 'exit'):
 		exit()
 	global store
-	store = user_input('store')
-	if (store =='exit'):
-		exit()
-	len_of_items = len(file[day]) #to identify if the foor loop is iterated throughthou the avaialable keys
-
-
+	
+	 #to identify if the foor loop is iterated throughthou the avaialable keys
+	cofday = 1
+	lofday = len(file)
+	print ("number of days inside json " + "{}".format(lofday), end="\n")
 	for wday, jstore in file.items():
 		if(wday==day):
+			store = user_input('store')
+			if (store =='exit'):
+				exit()
+			len_of_days = (len(file[day]))
 			print (day,end="\n")
+
 			for keys, info in jstore.items():
 				if(keys==store):
-					print(store,end="\n")
-				else:
-					count = count+1
+					#print (file[day][store][item])
+					#food_items = None
+					#item = 'item'
+					#food_items = file[day][store][item].keys()							
+					#print (food_items)
+
+					#fooditem = user_input('food_item')
+					#if (store =='exit'):
+					#	exit()
+					#lofitem = (len(file[day][keys]["item"]))
 					continue
-			if(count == len_of_items):
-				file[wday][store] = {}
-				stores.append(store)
+	
+				else:
+					stcount = stcount+1
+					continue
 
 		else:
+			cofday=cofday +1
 			continue
-
 			
-	'''		
-	wait_time = user_input('wait_time')
-	if(wait_time == 'exit'):
+	if (cofday > lofday):
+		file[day]={}
+		print("new day created " + day, end="\n")
+		write_to_file(file)
+	if(stcount > len_of_days):
+		file[day][store] = {}
+		stores.append(store)
+		write_to_file(file)
+
+	#start inputting for fitem
+	fooditem = user_input('food_item')
+	if (fooditem =='exit'):
 		exit()
-	else:
-		file[wday][store]["Waiting Time"] = wait_time
+	fromtime = user_input('fromtime')
+	if (fromtime =='exit'):
+		exit()
+	totime = user_input('totime')
+	if (totime =='exit'):
+		exit()
+	price = user_input('price')
+	file[day][store]["item"] = {}
+	file[day][store]["item"][fooditem] = {}
+	file[day][store]["item"][fooditem]["from"] = fromtime
+	file[day][store]["item"][fooditem]["till"] = totime
+	file[day][store]["item"][fooditem]["price"] = price
 
-			
-		try:
-			choice = input("Enter      \n1 -  for updating waiting time\n 2 - for updating working hrs \n 3 - for updating item info\n")
-			if(choice.isdigit):
-				for selection in option:
-					if (choice == option):
-								user_input(option[selection])
-	'''
-	#opening file to write
+	write_to_file(file)
+	input_to_json()
+
+
+	
+
+def write_to_file(file):
 	file_handle = open ("test.json", mode="w", encoding="utf-8")
 	json.dump(file, file_handle, indent=4 )
 	file_handle.close()
+	file_handle = open ("test.json", mode="r", encoding="utf-8")
+	file = json.load(file_handle)
+	file_handle.close()
+
 	#closed the file
 
 
